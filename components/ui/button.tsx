ba8baf95c2a820bbd848ca3219ed1ac8ec156296
Noe Krebs <eon.sberk@gmail.com>
2025-10-21 20:11:43 +0200

    fix: change button color

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -10,7 +10,7 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          "bg-gradient-to-t from-primary to-emerald-200 shadow-primary/20 shadow-md text-primary-foreground hover:bg-primary/90",
+          "bg-gradient-to-t bg-primary shadow-primary/20 shadow-lg text-primary-foreground hover:bg-primary/90",
         destructive:
           "bg-destructive text-white hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:
