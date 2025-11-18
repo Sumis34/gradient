b3781177c8b010d034fc7a711024324d411dfbc9
Noe Krebs <eon.sberk@gmail.com>
2025-11-18 17:38:29 +0100

    feat: add semester cards

--- app/app/semester/page.tsx
+++ app/app/semester/page.tsx
@@ -1,3 +1,35 @@
+"use client";
+
+import { Card, CardHeader } from "@/components/ui/card";
+import { useCollections } from "@/context/collection-context";
+import { backgroundIndex } from "@/lib/utils";
+import { useLiveQuery } from "@tanstack/react-db";
+
 export default function SemestersPage() {
-  return <></>;
+  const { semesters: semestersCollection } = useCollections();
+
+  const { data: semesters } = useLiveQuery((q) =>
+    q.from({ semester: semestersCollection }).select(({ semester }) => ({
+      id: semester.id,
+      name: semester.name,
+      description: semester.description,
+    }))
+  );
+
+  return (
+    <div className="grid md:grid-cols-3">
+      {semesters?.map((semester) => {
+
+        return (
+          <Card key={semester.id} className="p-0">
+            <CardHeader></CardHeader>
+            <div>
+              <h2>{semester.name}</h2>
+              <p>{semester.description}</p>
+            </div>
+          </Card>
+        );
+      })}
+    </div>
+  );
 }
